cb57c30967add560620467c215eff4bd0ac27f5d
Sherwan <shemo203@student.liu.se>
2025-04-09 23:08:35 +0200

    added basic flask server

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
-from student import Student
-from teacher import Teacher
-from document import Document
-from analyzer_api import Analyzer
-from ThresholdChecker import ThresholdChecker
-from submission import Submission
+from flask import Flask
+from waitress import serve
 
-test_student = Student("1", "Abdi")
-doc = Document("This is a dummy text")
-test_teacher = Teacher("1", "Khaliq")
+app = Flask(__name__)
 
-score = Analyzer(doc).analyze()
-threshold_result = ThresholdChecker(0.5)
-threshold_result.check_score(score)
-submit = Submission(score, doc, test_teacher)
+@app.route("/")
+@app.route('/index')
+def index():
+    return "Hello world!"
 
-
-
-
-
-
-
+if __name__ == "__main__":
+    serve(app, host="0.0.0.0", port = 8000)
