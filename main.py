50a92e264e7cdb246a1966014c24f33fff9705f6
Sherwan <shemo203@student.liu.se>
2025-04-24 16:48:36 +0200

    finished file form upload locally

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import os
 from waitress import serve
-
+UPLOAD_FOLDER = 'uploads'
 app = Flask(__name__)
 
-@app.route("/")
-@app.route("/index")
+@app.route("/", methods=['GET','POST'])
 def submit():
-    return render_template("submitpage.html")
+    if request.method == 'POST':  
+        return submit_file()
+    else:
+        return render_template("submitpage.html")
+
+
+def submit_file():
+    file = request.files['file']
+    file.save(os.path.join(UPLOAD_FOLDER, file.filename))
+    return f"file {file.filename} returned sucessfully!"
 
 
 if __name__ == "__main__":
     print("running")        
     app.run(debug=True)
+
+
