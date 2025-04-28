f6a337638cf033d587ab1c80ecfe96611722d625
Sherwan <shemo203@student.liu.se>
2025-04-28 23:26:03 +0200

    added pymupdf pdf extraction and AI detection checker

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from flask import Flask, render_template, request
 import os
 from waitress import serve
+from transformers import pipeline
+import pymupdf
+import requests
+from gradio_client import Client
+
+#reminders: use transformers and pumypdf for AI detection.
+
 UPLOAD_FOLDER = 'uploads'
+OUTPUT_FOLDER = 'outputs'
 app = Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
@@ -15,7 +23,24 @@ def submit():
 def submit_file():
     file = request.files['file']
     file.save(os.path.join(UPLOAD_FOLDER, file.filename))
-    return f"file {file.filename} returned sucessfully!"
+    text = extract_pdf_content(file.filename)
+    return f"Content from PDF: {analyze_text(text)} returned sucessfully!"
+
+def extract_pdf_content(pdf_input):
+    doc = pymupdf.open(f"uploads/{pdf_input}")
+    out = ""
+    for page in doc:
+        out += page.get_text()
+    return out
+
+def analyze_text(text):
+    client = Client("yuchuantian/AIGC_text_detector")
+    result = client.predict(
+		text,
+		api_name="/predict_en")
+    return result
+
+
 
 
 if __name__ == "__main__":
