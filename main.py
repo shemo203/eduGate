ef6726b6b987e278e78e18b1916e54a9032e71b1
Sherwan <shemo203@student.liu.se>
2025-04-30 18:22:26 +0200

    added some logic

--- main.py
+++ main.py
@@ -14,17 +14,21 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
 def submit():
-    if request.method == 'POST':  
+    if request.method == 'POST': 
         return submit_file()
     else:
         return render_template("submitpage.html")
+
 
 
 def submit_file():
     file = request.files['file']
     file.save(os.path.join(UPLOAD_FOLDER, file.filename))
     text = extract_pdf_content(file.filename)
-    return f"Content from PDF: {analyze_text(text)} returned sucessfully!"
+    ai = analyze_text(text)
+    if ai[0] == "AI":
+        return render_template("aisubmitpage.html", message = ai[1])
+    return f"Document submitted: {analyze_text(text)}"
 
 def extract_pdf_content(pdf_input):
     doc = pymupdf.open(f"uploads/{pdf_input}")
